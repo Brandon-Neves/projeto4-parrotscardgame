e373e08f97568541c80ec4e9563e49111efbe8ee
Brandon Neves <dev.brandon.neves>
2022-06-20 15:29:05 -0300

    Implementado a função do prompt dentro do laço while, sempre que o usuario digitar numeros impares menores que 4 e maiores que 14, o prompt será mostrado até que o usuario digite os números válidos

---
+++ script.js
@@ -0,0 +1,26 @@
+const imagens = [
+  'imagens/bobrossparrot.gif',
+  'imagens/explodyparrot.gif',
+  'imagens/fiestaparrot.gif',
+  'imagens/metalparrot.gif',
+  'imagens/revertitparrot.gif',
+  'imagens/tripletsparrot.gif',
+  'imagens/unicornparrot.gif'
+]
+
+let quantidadeCartas
+let contador = 0
+
+quantidadeCartas = prompt('Olá! Com quantas cartas você quer jogar?')
+quantidadeCartas = Number(quantidadeCartas)
+console.log(quantidadeCartas)
+while (
+  quantidadeCartas % 2 !== 0 ||
+  quantidadeCartas > 14 ||
+  quantidadeCartas < 4
+) {
+  quantidadeCartas = prompt('Digite um número que seja par entre 4 e 14')
+  quantidadeCartas = Number(quantidadeCartas)
+}
+
+console.log('Você agora está pronto para jogar')
