405f3793b3bdc03cd81943ce8a8eb07809356906
Brandon Neves <dev.brandon.neves>
2022-06-20 16:38:32 -0300

    Implementado o layout desktop e layout mobile, ambos responsivos no arquivo style.css

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const imagens = [
 let quantidadeCartas
 let contador = 0
 
+function comparador() {
+  return Math.random() - 0.5
+}
+
 quantidadeCartas = prompt('Olá! Com quantas cartas você quer jogar?')
 quantidadeCartas = Number(quantidadeCartas)
 console.log(quantidadeCartas)
@@ -22,5 +26,4 @@ while (
   quantidadeCartas = prompt('Digite um número que seja par entre 4 e 14')
   quantidadeCartas = Number(quantidadeCartas)
 }
-
 console.log('Você agora está pronto para jogar')
